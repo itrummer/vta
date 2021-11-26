55b4503c9abb8b6a85e51c767d1f7443f36b3bf3
itrummer <immanuel.trummer@gmail.com>
2021-11-26 16:30:24 -0500

    Polished presentation, added rate limit and session ID.

--- src/front/main.py
+++ src/front/main.py
@@ -4,10 +4,32 @@ Created on Nov 25, 2021
 @author: immanueltrummer
 '''
 import json
+import random
 import requests
 import streamlit as st
+import string
+import time
 
-st.set_page_config(page_title='Virtual Teaching Assistant')
+st.set_page_config(
+    page_title='Virtual Teaching Assistant',
+    layout='wide')
+
+def check_rate():
+    """ Checks rate of query generation.
+    
+    Returns:
+        true iff the query rate is acceptable
+    """
+    cur_s = time.time()
+    init_s = st.session_state['init_s']
+    elapsed_s = cur_s - init_s
+    query_nr = st.session_state['query_nr']
+    if query_nr == 0:
+        return True
+    elif elapsed_s / query_nr < 10:
+        return False
+    else:
+        return True   
 
 @st.cache(allow_output_mutation=True)
 def generate_answer(question):
@@ -19,9 +41,25 @@ def generate_answer(question):
     Returns:
         dictionary with answer properties
     """
-    http_r = requests.get(answer_url, params={'question':question})
-    r_dict = json.loads(http_r.text)
-    return r_dict
+    if not check_rate():
+        return {'error':True, 'answer':'Error - reached query rate limits.'}
+    else:
+        http_r = requests.get(answer_url, params={
+            'question':question, 'user_id':st.session_state['user_id']})
+        r_dict = json.loads(http_r.text)
+        st.session_state['query_nr'] = st.session_state['query_nr'] + 1
+        return r_dict
+
+def generate_id(length):
+    """ Generate random ID string with given length.
+    
+    Args:
+        length: length of ID string
+    
+    Returns:
+        randomly generated ID string
+    """
+    return ''.join(random.choice(string.ascii_letters) for _ in range(length)) 
 
 @st.cache(allow_output_mutation=True)
 def register_feedback(feedback):
@@ -30,18 +68,23 @@ def register_feedback(feedback):
     Args:
         feedback: dictionary with feedback
     """
+    feedback['user_id'] = st.session_state['user_id']
     http_r = requests.get(feedback_url, params=feedback)
     return http_r.text
 
 answer_url = st.secrets['answer_url']
 feedback_url = st.secrets['feedback_url']
 correct_password = st.secrets['password']
-
+if 'user_id' not in st.session_state:
+    st.session_state['user_id'] = generate_id(48)
+    st.session_state['init_s'] = time.time()
+    st.session_state['query_nr'] = 0
+ 
 password = st.text_input('Enter password:', type='password')
 if password == correct_password:
 
-    st.header('"Virtual Teaching Assistant" for the Database Lecture')
-    
+    #st.header('"Virtual TA" for the Database Lecture by Prof. Trummer')
+    st.header('Ask Questions about Database Systems')
     st.markdown('''
         This tool answers natural language questions about the course material 
         taken from [the database lecture](http://www.databaselecture.com). Answers 
@@ -51,9 +94,10 @@ if password == correct_password:
         
         Example questions:
         - Explain this SQL query: SELECT Avg(age) FROM Customers GROUP BY zip_code
-        - What is the second rule of write-ahead logging? 
-        - What is a strict schedule?
+        - What does the rectangle represent in ER diagrams?
+        - What is a strict schedule? 
         ''')
+    #- What is the second rule of write-ahead logging?
     
     question = st.text_input('Enter your question:', max_chars=200)
     
